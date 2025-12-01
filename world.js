d95302187cc9c5b0160c7ff2e3046f55edcab2cb
Ajani Henry <henryajani.ah@gmail.com>
2025-11-30 23:34:22 -0500

    Changed country search output to be in table. Started City search and removed accidentally added file.

--- world.js
+++ world.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded',() =>{
-    const lookup_button = document.getElementById('lookup');
+    const lookup_button1 = document.getElementById('lookup1'); // country
+    const lookup_button2 = document.getElementById('lookup2'); // city
     const resultdiv = document.getElementById('result');
-    const country_input = document.getElementById('country');
+    const search_input = document.getElementById('search');
 
-    lookup_button.addEventListener('click',() =>{
-        const country = country_input.value.trim(); // get user input
+    lookup_button1.addEventListener('click',() =>{
+        const country = search_input.value.trim(); // get user input
 
-        fetch(`world.php?country=${encodeURIComponent(country)}`)
+        fetch(`world.php?country=${encodeURIComponent(search)}`)
+            .then(response => response.text())
+            .then(data =>{
+                resultdiv.innerHTML = data; // Display results in the html div
+            })
+            .catch(error =>{
+                console.error('Error fetching data:', error);
+                resultdiv.innerHTML = '<p>Error fetching data.</p>';
+            });
+    });
+
+    lookup_button2.addEventListener('click',() =>{
+        const search = search_input.value.trim(); // get user input
+
+        fetch(`world.php?city=${encodeURIComponent(search)}`)
             .then(response => response.text())
             .then(data =>{
                 resultdiv.innerHTML = data; // Display results in the html div
--- world.php
+++ world.php
@@ -18,8 +18,23 @@ if(isset($_GET['country']) && $_GET['country'] !== ''){ // checks country parame
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+  <thead>
+        <tr>
+            <th>Country Name</th> 
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+  <tbody>
+    <?php foreach ($results as $row): ?>
+      <tr>
+          <td><?= htmlspecialchars($row['name']); ?></td>
+          <td><?= htmlspecialchars($row['continent']); ?></td>
+          <td><?= htmlspecialchars($row['independence_year']); ?></td>
+          <td><?= htmlspecialchars($row['head_of_state']); ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
